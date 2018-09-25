1e2967845b2e708df74b8b99bb3c834ffd776f86
Graham Ingokho <janikibichi@gmail.com>
2018-09-25 15:29:22 +0300

    All tests pass

--- src/main/scala/SumOfMultiples.scala
+++ src/main/scala/SumOfMultiples.scala
@@ -1,6 +1,13 @@
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int = {
+    val naturals = (1 until limit).toSet
 
-    1
+    val toSum = for {
+      i <- factors
+      j <- naturals
+      if(j%i == 0)
+    }yield j
+
+    toSum.sum
   }
 }
